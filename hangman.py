9b2d02d0c02d79dfd9b5d97083698bf63b396c00
Sameer Mesiah <sameer-mesiah@hotmail.com>
2022-07-20 13:00:47 +0100

    Re-factored code

--- hangman.py
+++ hangman.py
@@ -1,24 +1,99 @@
 import random
 
 class Game:
-    def __init__(self):
-        self.lives = 6
-        self.words = list(line.strip() for line in open('words.txt'))
-        self.game_end = False
-        self.game_won = False
+    words = list(line.strip() for line in open('words.txt'))
 
-    def select_word(self):
-        return self.words[random.randint(0, len(self.words) -1)]
-
-    def convert_word (self,word):
+    def get_word_from_list(self):
+        word = self.words[random.randint(0, len(self.words) - 1)]
         letters = []
         for letter in word:
             letters.append([letter, False])
         return letters
 
 
+    def __init__(self):
+        self.__lives_left = 6
+        self.__game_letters = self.get_word_from_list()
+        self.__game_state = {"ended":False, "won": False}
+
+    def get_lives (self):
+        return self.__lives_left
+
+    def set_lives (self, lives_left):
+        self.__lives_left = lives_left
+
+    def get_game_letters (self):
+        return self.__game_letters
+
+    def set_game_letters (self, letters):
+        self.__game_letters = letters
+
+    def get_game_state (self):
+        return self.__game_state
+
+    def set_game_state (self, state):
+        self.__game_state = state
+
+    def win (self):
+        for letter in self.get_game_letters():
+            if not letter[1]:
+                return False
+        return True
+
+    def correct_guess (self, guess):
+        for letter in self.get_game_letters():
+            if guess.lower() == letter[0].lower():
+                return True
+        return False
+
+    def has_game_ended(self):
+        if self.get_lives() == 0:
+            return True
+        if self.win():
+            return True
+        return False
+
+    def play_game(self):
+        game_display = Display()
+        while not self.__game_state["ended"]:
+
+            lives = self.get_lives()
+            letters = self.get_game_letters()
+
+            print(game_display.get_hangman(lives))
+            print(game_display.get_letters(letters))
+
+            guess = input("Enter a letter:")
+
+            if self.correct_guess(guess):
+                for letter in letters:
+                    if guess.lower() == letter[0].lower():
+                        letter[1] = True
+                print ("Correct Guess")
+            else:
+                self.set_lives(lives-1)
+                lives = self.get_lives()
+
+                print("Wrong Guess")
+
+                if lives == 1:
+                    print("You have 1 life left")
+                else:
+                    print(f"You have {lives} live(s) left!")
+
+            if self.has_game_ended():
+                if self.win():
+                    print(game_display.get_letters(letters))
+                    print("You have won.")
+                    self.set_game_state({"ended": True, "won": True})
+                else:
+                    print(game_display.get_hangman(lives))
+                    print("You have lost.")
+                    self.set_game_state({"ended": True, "won": False})
+
+
 class Display:
-    def show_hangman (self, lives):
+    def get_hangman(self, lives):
 
         hangmen = ["    +---+\n" + "        |\n" + "        |\n" + "        |\n",
                    "    +---+\n" + "    O   |\n" + "        |\n" + "        |\n",
@@ -28,18 +103,15 @@ class Display:
                    "    +---+\n" + "    O   |\n" + "  / | \ |\n" + "   /    |\n",
                    "    +---+\n" + "    O   |\n" + "  / | \ |\n" + "   / \  |\n"]
 
-        print (hangmen[len(hangmen)-lives-1])
+        return hangmen[len(hangmen) - lives - 1]
 
-    def show_letters (self, letters):
-
+    def get_letters(self, letters):
+        letters_string = ""
 
         for letter in letters:
-            if letter[1] == True:
-                print(letter[0].upper(), end=' ')
+            if letter[1]:
+                letters_string += letter[0].upper()
             else:
-                print ("_",end=' ')
-        print ("\n")
+                letters_string += "_"
+        return letters_string
 
-    def show_lives(self, lives):
-
-        print (f"You have {lives} lives left!")
