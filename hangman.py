eb501cc5bfdf96a500baee697bd0ad36f6ccdc28
Sameer Mesiah <sameer-mesiah@hotmail.com>
2022-07-20 13:12:41 +0100

    Added comments

--- hangman.py
+++ hangman.py
@@ -1,52 +1,61 @@
 import random
 
+
 class Game:
+    # Load list of words from text document.
     words = list(line.strip() for line in open('words.txt'))
 
     def get_word_from_list(self):
+
+        # Random word is selected from the list.
         word = self.words[random.randint(0, len(self.words) - 1)]
+
         letters = []
+
+        # Word is converted into a list of lists representing letters that have been guessed/not guessed.
         for letter in word:
             letters.append([letter, False])
         return letters
 
-
     def __init__(self):
         self.__lives_left = 6
         self.__game_letters = self.get_word_from_list()
-        self.__game_state = {"ended":False, "won": False}
+        self.__game_state = {"ended": False, "won": False}
 
-    def get_lives (self):
+    def get_lives(self):
         return self.__lives_left
 
-    def set_lives (self, lives_left):
+    def set_lives(self, lives_left):
         self.__lives_left = lives_left
 
-    def get_game_letters (self):
+    def get_game_letters(self):
         return self.__game_letters
 
-    def set_game_letters (self, letters):
+    def set_game_letters(self, letters):
         self.__game_letters = letters
 
-    def get_game_state (self):
+    def get_game_state(self):
         return self.__game_state
 
-    def set_game_state (self, state):
+    def set_game_state(self, state):
         self.__game_state = state
 
-    def win (self):
+    def win(self):
         for letter in self.get_game_letters():
+            # If at least one letter has not been correctly guessed, game has not been won
             if not letter[1]:
                 return False
         return True
 
-    def correct_guess (self, guess):
+    def correct_guess(self, guess):
         for letter in self.get_game_letters():
             if guess.lower() == letter[0].lower():
                 return True
         return False
 
     def has_game_ended(self):
+
+        # Game ends when no lives remain or all letters have been guessed correctly.
         if self.get_lives() == 0:
             return True
         if self.win():
@@ -54,7 +63,13 @@ class Game:
         return False
 
     def play_game(self):
+
+        print ("Hangman is a simple word guessing game. Players try to figure out an unknown word by guessing letters.\n"
+               "If too many letters which do not appear in the word are guessed, the player is hanged (and loses).\n"
+               "In this version of the game, you are allowed 6 incorrect guesses before you lose. \n")
+
         game_display = Display()
+
         while not self.__game_state["ended"]:
 
             lives = self.get_lives()
@@ -69,9 +84,10 @@ class Game:
                 for letter in letters:
                     if guess.lower() == letter[0].lower():
                         letter[1] = True
-                print ("Correct Guess")
+
+                print("Correct Guess")
             else:
-                self.set_lives(lives-1)
+                self.set_lives(lives - 1)
                 lives = self.get_lives()
 
                 print("Wrong Guess")
@@ -81,7 +97,10 @@ class Game:
                 else:
                     print(f"You have {lives} live(s) left!")
 
+            # Determine if game has ended.
             if self.has_game_ended():
+
+                # And if it has ended, determine if player has won or lost.
                 if self.win():
                     print(game_display.get_letters(letters))
                     print("You have won.")
@@ -113,5 +132,5 @@ class Display:
                 letters_string += letter[0].upper()
             else:
                 letters_string += "_"
-        return letters_string
 
+        return letters_string
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import hangman
 
 new_game = hangman.Game()
-print(new_game.get_game_letters())
 new_game.play_game()
